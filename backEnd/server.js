55b73626307ba7c2b86c9548a1a914333f7fba98
Prajjawal1997 <105428192+Prajjawal1997@users.noreply.github.com>
2025-08-05 18:48:43 +0530

    Update server.js
    Removed cors error

--- backEnd/server.js
+++ backEnd/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import productRoute from "./routes/product.routes.js"
+import cors from 'cors';
 
 
 dotenv.config();
@@ -10,6 +11,14 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+const allowedOrigins = ['https://product-store-frontend-gv0a.onrender.com'];
+
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 app.use(express.json());
 
